8cd34d2a912a2eab463095200c498471b99edc51
obwansan <kob123@hotmail.co.uk>
2018-03-28 15:45:32 +0100

    First commit - created files

---
+++ timer.js
@@ -0,0 +1,21 @@
+/*
+* Create a button that starts a timer, creates another button and the new button disappears after 3 seconds.
+* The button that starts a timer stays in place.
+* */
+
+
+// button starts a timer
+
+
+
+// same button creates another button
+
+
+
+// new button disappears after 3 seconds
+
+
+
+
+
+
