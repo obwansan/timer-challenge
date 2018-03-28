e92746c80e67b809769039588a15b49a3580dc7c
obwansan <kob123@hotmail.co.uk>
2018-03-28 16:01:17 +0100

    Starts timer and creates new button after 3 seconds

--- timer.js
+++ timer.js
@@ -3,12 +3,17 @@
 * The button that starts a timer stays in place.
 * */
 
+var createBtn = document.querySelector('#createBtn');
+var newBtnContainer = document.querySelector('#newBtnContainer');
 
 // button starts a timer
+createBtn.addEventListener('click', function() {
+    setTimeout(function() {
+        // button creates another button
+        newBtnContainer.innerHTML = '<input type="submit" value="New button!" id="newBtn">';
+    }, 3000)
+});
 
-
-
-// same button creates another button
 
 
 
