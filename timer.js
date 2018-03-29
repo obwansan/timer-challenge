cff95e24e773e6b81d164244948274351d466fdc
obwansan <kob123@hotmail.co.uk>
2018-03-29 08:22:01 +0100

    counter displays number of clicks on the button in the 3 seconds

--- timer.js
+++ timer.js
@@ -7,18 +7,33 @@
 
 var createBtn = document.querySelector('#createBtn');
 var newBtnContainer = document.querySelector('#newBtnContainer');
+var clicks = document.querySelector('#clicks');
+var counter = 0;
 
 // button starts a timer
 createBtn.addEventListener('click', function() {
+
     setTimeout(function() {
-        // button creates another button
+
+        // button creates another button (after 3 seconds)
         newBtnContainer.innerHTML = '<input type="submit" value="New button!" id="newBtn">';
+
+        // get number of clicks on the button in the 3 seconds (event bubbles to parent div)
+        newBtnContainer.addEventListener('click', function() {
+            counter++;
+        });
+
         // new button disappears after 3 seconds
         setTimeout(function() {
             newBtnContainer.innerHTML = '';
+
+            // display number of button clicks in the 3 seconds
+            clicks.innerHTML = '<p>Button clicks in 3 seconds: ' + counter + '</p>';
         }, 3000);
+
     }, 3000);
 });
+
 
 
 
