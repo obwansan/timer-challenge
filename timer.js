90939a610f466b29f1a4acd8b6b6a96cb22d2d0e
obwansan <kob123@hotmail.co.uk>
2018-03-29 07:59:48 +0100

    new button disappears after 3 seconds

--- timer.js
+++ timer.js
@@ -1,6 +1,8 @@
 /*
 * Create a button that starts a timer, creates another button and the new button disappears after 3 seconds.
 * The button that starts a timer stays in place.
+*
+* Populate a counter with the number of clicks on the button in the 3 seconds.
 * */
 
 var createBtn = document.querySelector('#createBtn');
@@ -11,13 +13,19 @@ createBtn.addEventListener('click', function() {
     setTimeout(function() {
         // button creates another button
         newBtnContainer.innerHTML = '<input type="submit" value="New button!" id="newBtn">';
-    }, 3000)
+        // new button disappears after 3 seconds
+        setTimeout(function() {
+            newBtnContainer.innerHTML = '';
+        }, 3000);
+    }, 3000);
 });
 
 
 
 
-// new button disappears after 3 seconds
+
+
+
 
 
 
